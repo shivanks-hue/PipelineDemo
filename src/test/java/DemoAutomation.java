0ae2257afa15b261d06c8b7a0617244456405511
shivanks-hue <shivanks@lambdatest.com>
2025-10-06 21:36:25 +0530

    Automation file changed

--- src/test/java/DemoAutomation.java
+++ src/test/java/DemoAutomation.java
@@ -2,53 +2,54 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
-import java.util.Map;
-
 
 public class DemoAutomation {
-    public static void main(String[] args) {
+
+    RemoteWebDriver driver;
+
+    @BeforeTest
+    public void setup() throws MalformedURLException {
         String username = "shivanks";
         String accessKey = "LT_V6UEGtFRGiIPH4QcUdNKQv4RIetJjVfXKKIEkrP3Sk15opJ";
-        RemoteWebDriver driver = null;
         String gridURL = "@hub.lambdatest.com/wd/hub";
+
         ChromeOptions browserOptions = new ChromeOptions();
         browserOptions.setPlatformName("Windows 10");
         browserOptions.setBrowserVersion("140.0");
-        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
-        ltOptions.put("username", "shivanks");
-        ltOptions.put("accessKey", "LT_V6UEGtFRGiIPH4QcUdNKQv4RIetJjVfXKKIEkrP3Sk15opJ");
+
+        HashMap<String, Object> ltOptions = new HashMap<>();
+        ltOptions.put("username", username);
+        ltOptions.put("accessKey", accessKey);
         ltOptions.put("video", true);
         ltOptions.put("build", "Java_Selenium_Script");
         ltOptions.put("project", "DemoOnAutomation");
         ltOptions.put("selenium_version", "4.0.0");
         ltOptions.put("w3c", true);
+
         browserOptions.setCapability("LT:Options", ltOptions);
 
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + gridURL), browserOptions);
+    }
 
-        try {
-            driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + gridURL), browserOptions);
+    @Test
+    public void runDemoAutomation() {
+        driver.get("https://ecommerce-playground.lambdatest.io/");
 
+        WebElement iphoneLink = driver.findElement(By.xpath("//a[contains(text(), 'iPhone')]"));
+        iphoneLink.click();
+    }
 
-            // Navigate to the webpage
-            driver.get("https://ecommerce-playground.lambdatest.io/");
-
-
-            // Find the element with the name iPhone
-            WebElement iphoneLink = driver.findElement(By.xpath("//a[contains(text(), 'iPhone')]"));
-
-
-            // Click on the product to visit the product page
-            iphoneLink.click();
-
-            // Close the browser
-            driver.close();
-        } catch (MalformedURLException e) {
-            System.out.println("Invalid grid URL");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
         }
     }
 }
